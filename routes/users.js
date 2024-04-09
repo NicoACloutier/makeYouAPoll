a2820011bc101898d2ba2237c1289155ea0f2f33
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2024-04-09 10:11:25 -0400

    finalize initial user database API

--- routes/users.js
+++ routes/users.js
@@ -25,10 +25,21 @@ function createSalt() {
 }
 
 /*
-Return the current number of users stored in the `users` table.
+Return the users stored in the `users` table.
 */
 router.get('/', (request, response) => {
     pool.query('SELECT * FROM users', [], (error, results) => {
+        if (error) throw error;
+        response.status(200).json(results.rows);
+    });
+});
+
+/*
+Return a user stored in the `users` table given an id.
+*/
+router.get('/:id', (request, response) => {
+    const id = parseInt(request.params.id);
+    pool.query('SELECT * FROM users WHERE user_id = $1', [id], (error, results) => {
         if (error) throw error;
         response.status(200).json(results.rows);
     });
@@ -39,11 +50,21 @@ router.get('/', (request, response) => {
 Create a user and add to the `users` table.
 */
 router.post('/', (request, response) => {
-    const { email, password } = parseInt(request.params.id);
+    const { email, password } = request.body;
     const id = getNumUsers();
     const salt = createSalt();
     const hash = createHash('sha256').update(password + salt).digest('hex');
     pool.query('INSERT INTO users (id, salt, hash, email) VALUES ($1, $2, $3, $4) RETURNING *', [id, salt, hash, email], (error, results) => {
+        if (error) throw error;
+    });
+});
+
+/*
+Delete a user given an id.
+*/
+router.delete('/:id', (request, response) => {
+    const id = parseInt(request.params.id);
+    pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
         if (error) throw error;
     });
 });
