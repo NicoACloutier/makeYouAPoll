db2d117be72dffd7302ea02294706d37804b46a5
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2024-05-05 21:57:32 -0400

    add information to home screen

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { useState } from 'react';
 
+const SERVER_PORT = 3000;
+
 function Home() {
     const [users, setUsers] = useState(false);
 
+    async function getUser() {
+        const response = await fetch(`http://localhost:${SERVER_PORT}/auth`, {
+            method: 'GET',
+        });
+        if (response === undefined) { return { name: undefined }; }
+        return response.json();
+    }
+
     return (
         <div className="App">
-            <label id="notification">Home</label>
+            <label id="notification" value={getUser()["name"]}></label>
         </div>
     );
 }
