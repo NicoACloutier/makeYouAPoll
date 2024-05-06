73a61755d8cdc4d2007929d325c2d4c723f763b9
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2024-05-05 21:57:05 -0400

    adapt `Login.js` to server-side authentication

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,47 +1,20 @@
 import React from 'react';
 import { useState } from 'react';
 import '../App.css';
-import { createHash } from 'crypto';
 
 const SERVER_PORT = 3000;
-
-function informationMatches(enteredPassword, salt, hash) {
-    const newHash = createHash('sha256').update(enteredPassword + salt).digest('hex');
-    return newHash === hash;
-}
 
 function Login() {
     const [users, setUsers] = useState(false);
 
-    function getUser() {
-        fetch(`http://localhost:${SERVER_PORT}`).then(response => {
-            return response.text();
-        }).then(data => {
-            setUsers(data);
-        });
-    }
-    
-    async function findUser(email) {
-        const response = await fetch(`http://localhost:${SERVER_PORT}/users?email=${email}`, {
-            method: 'GET',
-        });
-        if (response === undefined) { return undefined; }
-        return response.json();
-    }
-    
     async function login() {
         const email = document.getElementById("email").value;
         const enteredPassword = document.getElementById("password").value;
-        const userInfo = await findUser(email);
-        if (userInfo === undefined) { 
-            document.getElementById("notification").innerHTML = "No user found.";
-            return;
-        }
-        
-        if (informationMatches(enteredPassword, userInfo["salt"], userInfo["hash"])) {
-            document.getElementById("notification").innerHTML = "Success.";
-        }
-        else { document.getElementById("notification").innerHTML = "No match."; }
+        const response = await fetch(`http://localhost:${SERVER_PORT}/auth/in`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', },
+            body: JSON.stringify({ email, enteredPassword }),
+        });
     }
     
     return (
