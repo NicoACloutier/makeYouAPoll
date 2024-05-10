0edbaebc26ec106dce67c43a8a428908a9569616
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2024-05-10 17:52:55 -0400

    properly display information on homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,24 +3,33 @@ import { useState } from 'react';
 
 const SERVER_PORT = 3000;
 
-function Home() {
-    const [users, setUsers] = useState(false);
+async function getUser() {
+    const response = await fetch(`http://127.0.0.1:${SERVER_PORT}/auth`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json', },
+        credentials: 'include'
+    });
+    if (response === undefined || response.status !== 200) { return { name: undefined }; }
+    return response.json();
+}
 
-    async function getUser() {
-        const response = await fetch(`http://127.0.0.1:${SERVER_PORT}/auth`, {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json', },
-            credentials: 'include'
-        });
-        if (response === undefined || response.status !== 200) { return { name: undefined }; }
-        return response.json();
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { id: undefined, name: undefined, email: undefined };
     }
-
-    return (
-        <div className="App">
-            <label id="notification" value={getUser()["name"]}></label>
-        </div>
-    );
+    
+    componentDidMount() {
+        getUser().then(ret => this.setState(ret));
+    }
+    
+    render() {
+        return (
+            <div className="App">
+                <h1>{this.state.name}</h1>
+            </div>
+        );
+    }
 }
 
 export default Home;
