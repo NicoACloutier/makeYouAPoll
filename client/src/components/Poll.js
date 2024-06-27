5b9cf337c34bd096e2dd9103ec7c5194732fb731
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2024-06-27 11:36:04 -0400

    fix answer fetching on poll page

--- client/src/components/Poll.js
+++ client/src/components/Poll.js
@@ -6,7 +6,10 @@ const SERVER_PORT = 3000;
 
 async function getPoll(pollId) {
     const response = await fetch(`http://127.0.0.1:${SERVER_PORT}/polls?id=${pollId}`, { method: 'GET' });
-    return response;
+    const answers = await fetch(`http://127.0.0.1:${SERVER_PORT}/answers?poll_id=${pollId}`, { method: 'GET' });
+    const data = await response.json();
+    data.answers = (await answers.json()).map(x => x.answer_text);
+    return data;
 }
 
 function Poll() {
@@ -24,11 +27,11 @@ function Poll() {
     }, []);
     
     return (
-        <li>
+        <div className="App">
             <p>{question}</p>
             <ul>{answers.map(x => <li key={x}>{x}</li>)}</ul>
-        </li>
+        </div>
     );
 }
 
-export default Login;
+export default Poll;
